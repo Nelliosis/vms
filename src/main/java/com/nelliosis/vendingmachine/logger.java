bd5fdeb8fb8af09705004dea018a6f271f493741
Junel Capawing <jcapawing@gmail.com>
2022-06-15 17:39:15 +0800

    added tracestack string converter

--- src/main/java/com/nelliosis/vendingmachine/logger.java
+++ src/main/java/com/nelliosis/vendingmachine/logger.java
@@ -1,6 +1,9 @@
 package com.nelliosis.vendingmachine;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.util.logging.FileHandler;
 import java.util.logging.Formatter;
 import java.util.logging.Level;
@@ -8,8 +11,8 @@ import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 /*
- * My entire logging class came from this StackOverFlow thread.
- * All credits go to them.
+ * Parts of the logger class came from this StackOverFlow thread.
+ * Credits go to them.
  * Visit: https://stackoverflow.com/questions/20737880/java-logging-through-multiple-classes
  */
 
@@ -17,6 +20,7 @@ public class logger {
     static Logger log;
     public FileHandler fh;
     Formatter plainText;
+    static Writer writer = new StringWriter();
 
     public logger() throws IOException {
         // instance the logger and reset the manager
@@ -43,5 +47,11 @@ public class logger {
 
     public static void log(Level level, String msg) {
         getLogger().log(level, msg);
+    }
+
+    public static String TraceToString(Exception e) {
+        e.printStackTrace(new PrintWriter(writer));
+        String s = writer.toString();
+        return s;
     }
 }
